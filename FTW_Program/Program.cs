1faffa57afb1169136ec5fc3c20dbe3106658765
Alexandr Degtyarev <Sasha20111997@gmail.com>
2022-12-23 17:22:45 +0500

    Fill NewArray

--- FTW_Program/Program.cs
+++ FTW_Program/Program.cs
@@ -3,3 +3,19 @@ int Length = Math.Abs(int.Parse(Console.ReadLine()));
 
 string[] NewArray = new string[Length];
 int count = 0;
+
+for (int i = 0; i < NewArray.Length; i++)
+{
+    if (i < NewArray.Length)
+    {
+        Console.WriteLine($"Введите {i} элемент массива: ");
+        NewArray[i] = Console.ReadLine();
+        if (NewArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Введенный массив:" + "[" + String.Join(";", NewArray) + "]");

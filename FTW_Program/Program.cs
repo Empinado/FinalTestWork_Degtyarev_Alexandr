9e3f0bd8f64413bc73485fff7923e43b831c8cd3
Alexandr Degtyarev <Sasha20111997@gmail.com>
2022-12-23 17:21:10 +0500

    Added NewArray

--- FTW_Program/Program.cs
+++ FTW_Program/Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Введите числовое значение размерности массива : ");
 int Length = Math.Abs(int.Parse(Console.ReadLine()));
 
+string[] NewArray = new string[Length];
+int count = 0;
